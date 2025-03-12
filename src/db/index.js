a18a092205c75a970ac62d2ebda88bbbd2b3a3a7
Anup Pradhan <anuppradhan929@gmail.com>
2025-03-13 01:04:07 +0530

    connect to mongoDB

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../Constants.js";
+
+const connectDB = async() => {
+    try {
+        const connectionInstance =  await mongoose.connect(`${process.env.MONGOODB_URI}/ ${DB_NAME}`)
+        console.log(`MongoDB is connected to ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MongooDB is sowing ERROR", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
